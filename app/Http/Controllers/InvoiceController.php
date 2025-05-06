5a962c0c31270948ebc8cebfb6dbde3ce41224c8
ikhwanox <ikhwantukiran03@gmail.com>
2025-05-06 21:38:31 +0800

    improving invoice and gallery

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Facades\Log;
+use Illuminate\Support\Facades\Log;
 class InvoiceController extends Controller
 {
     /**
