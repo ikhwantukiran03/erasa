d6568d19e0e99f461165849b238dee5fa8b214b5
ikhwanox <ikhwantukiran03@gmail.com>
2025-05-07 19:58:08 +0800

    remove invoice for revamp

--- database/migrations/2025_05_05_140429_create_table_invoices.php
+++ database/migrations/2025_05_05_140429_create_table_invoices.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('booking_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['deposit', 'payment_1', 'payment_2', 'final_payment','other']);
             $table->string('invoice_path')->nullable();
             $table->dateTime('invoice_submitted_at')->nullable();
             $table->dateTime('invoice_verified_at')->nullable();
